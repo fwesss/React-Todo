d44ece93b7f15f375d9e08573433bd054a174f38
Westley Feller <feller.westley@gmail.com>
2019-10-31 20:26:43 -0700

    Build folder

---
+++ build/precache-manifest.dbaf9cf111ebdca902921cf82916f36b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199370eedcec283ed600aa39f9fc9d9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae8ffb0bd1fc172c48e",
+    "url": "/static/js/2.1e77db68.chunk.js"
+  },
+  {
+    "revision": "43db9ee165b8a648598f",
+    "url": "/static/js/main.3a366811.chunk.js"
+  },
+  {
+    "revision": "54bcc251cec63bea7899",
+    "url": "/static/js/runtime-main.a0a917f2.js"
+  }
+]);
